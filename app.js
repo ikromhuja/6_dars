f41f1119b075764e6c045a4263504ef37c76bdc3
ikrom marupov <ikrom@gmail.com>
2024-02-05 09:49:09 +0500

    Masala

---
+++ app.js
@@ -0,0 +1,86 @@
+// const son = +prompt('Sonni kiriting')
+
+// if (son > 0) {
+//     console.log(son + 1)
+// } else {
+//     console.log(son - 2)
+// }
+
+// let son = 457818754
+// son = son.toString();
+
+// let reverseNumber = "";
+
+// let i = son.length - 1
+
+// while (0 <= i) {
+//     reverseNumber += son[i];
+//     i--;
+// }
+
+// console.log(reverseNumber == +son)
+
+// let son = 8275;
+// son = son.toString();
+
+// let i = 0;
+// while (i < son.length) {
+//     if (son[i] % 2 != 0) {
+//         console.log(true)
+//         break
+//     }
+//     console.log(i);
+//     i++;
+// }
+
+
+/*   4.6 DARS */
+
+
+
+// let n = 5;
+// let counter = 0;
+
+// for (let i = 0; i <= n; i++) {
+//     console.log(`(${n} + ${i}) ** 2 = ${(n + i) ** 2}`);
+//     counter += (n + i) ** 2;
+// }
+// console.log("Sum: ", counter)
+
+/* ----------------------------------------------------------------------------------------------------------------- */
+
+// let n = 5
+// let pover = n
+// let sum = 0
+
+// for (let i = 1;  i <= n; i++) {
+//     sum += i ** pover;
+//     pover--;
+// }
+
+// console.log(sum);
+
+/* ---------------------------------------------------------------------------------------------------------------- */
+
+// let sum = "";
+
+// for (let i = 1; i <= 5; i++) {
+//     for (let k = 1; k <= i; k++) {
+//         sum += `${i} `;
+//     }
+//     console.log(sum.trimEnd());
+//     sum = "";
+// }
+
+/*----------------------------------------------------------------------------------------------------------------- */
+
+// let n = 20;
+// let counter = "";
+
+// for (let i = 1; i <= n; i++) {
+//     if (n % i == 0) {
+//         counter += `${i} `;
+//     }
+// }
+
+// console.log(counter.trimEnd())
